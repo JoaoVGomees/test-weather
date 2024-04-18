3c53770b4c9cf019e92a84493c03a321d44dbcb5
João <joaovitorsenac7818@gmail.com>
2024-04-17 21:45:37 -0300

    feat: criação do HTML e Css  e manipulação da API com JavaScript

---
+++ src/scripts/script.js
@@ -0,0 +1,23 @@
+const buscar = () => {
+
+  const cidade = document.querySelector('#nomeCidade').value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=777fd6c175f16899b669ab9b22be7638&units=metric`;
+
+  fetch(url)
+        .then(resposta =>{
+          console.log(resposta)
+          return resposta.json();
+        })
+          .then(dados =>{
+            console.log(dados);
+            document.querySelector('#city').value = dados.name;
+            document.querySelector('#tempe').value = dados.main.temp + 'º';
+          })
+          .catch(erro => {
+            console.log(erro);
+          })
+
+}
+
+
